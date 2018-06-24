f0f45860f94c62dc5cf36cf021149aeb36d2f507
Alberto <albertotrapiello@gmail.com>
2018-06-24 18:56:37 +0200

    Fin de definir las putas zonas

--- Brutal-Anal-Fisting/Brutal-Anal-Fisting/src/Region.cpp
+++ Brutal-Anal-Fisting/Brutal-Anal-Fisting/src/Region.cpp
@@ -17,8 +17,8 @@ Region::~Region()
 	if (z != NULL)
 	{
 		for (int i = 0; i < lenght; i++)
-			delete z[i];
-		delete z;
+			delete[] z[i];
+		delete[] z;
 	}
 }
 
@@ -41,23 +41,27 @@ void Region::read_file(std::string s)
 		while (!f.eof())
 		{
 			getline(f, aux1);
-			cout << " aux1:" << str;
 			if (s == aux1)
 			{
+				stringstream sstr1;
+				getline(f, aux1);
+				sstr1 << aux1;
+				sstr1>>lenght;
 				z = new Zones*[1];
-				lenght = 0;
-				while (str != "end")
+				for(int i=0; i<lenght;i++)
 				{
+					
 					stringstream sstr;
 					getline(f, str);
+					
 					sstr << str;
 					cout << "lenght=" << lenght << " str:" << str;
-					z[lenght] = new Zones;
-					sstr >> z[lenght]->x1 >> z[lenght]->y1 >> z[lenght]->x2 >> z[lenght]->y2;
-					cout << " x1:" << z[lenght]->x1 << " y1:" << z[lenght]->y1 << " x2:" << z[lenght]->x2 << " y2:" << z[lenght]->y2 << endl;
-					lenght++;
+					z[i] = new Zones;
+					sstr >> z[i]->x1 >> z[i]->y1 >> z[i]->x2 >> z[i]->y2;
+					cout << " x1:" << z[i]->x1 << " y1:" << z[i]->y1 << " x2:" << z[i]->x2 << " y2:" << z[i]->y2 << endl;
 				}
 			}
+			
 		}
 		f.close();
 	}
